c709ee3e6a16d6452961e24936c650c92f0339c8
Richard Lehane <richard.lehane@gmail.com>
2017-06-02 23:15:41 +1000

    /results JSON almost there

--- results.go
+++ results.go
@@ -5,10 +5,32 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strconv"
+	"strings"
 	"time"
 
+	"github.com/richardlehane/siegfried/pkg/core"
 	"github.com/richardlehane/siegfried/pkg/reader"
 )
+
+func getter(key string, fields [][]string) func(int, core.Identification) string {
+	mm := make(map[int]int)
+	for i, v := range fields {
+		for j, w := range v {
+			switch w {
+			case key, strings.ToUpper(key):
+				mm[i] = j
+			}
+		}
+	}
+	return func(idx int, id core.Identification) string {
+		f, ok := mm[idx]
+		if !ok {
+			return ""
+		}
+		return id.Values()[f]
+	}
+}
 
 var fileTitles = []string{"filename", "filesize", "modified", "errors"}
 
@@ -41,9 +63,15 @@ type Data struct {
 type Count map[string]int
 
 func (c Count) MarshalJSON() ([]byte, error) {
+	first := true
 	buf := &bytes.Buffer{}
 	buf.WriteByte('[')
 	for k, v := range c {
+		if first {
+			first = false
+		} else {
+			fmt.Fprint(buf, ",")
+		}
 		fmt.Fprintf(buf, "[\"%s\",%d]", k, v)
 	}
 	buf.WriteByte(']')
@@ -68,15 +96,15 @@ func results(r *http.Request) (*Results, error) {
 	if err != nil {
 		return nil, fmt.Errorf("no valid results; got %v", err)
 	}
-	//var hh bool
+	var hh bool
 	if len(file.Hash) > 0 {
-		//hh = true
+		hh = true
 		fileTitles = append(fileTitles, head.HashHeader)
 	}
 	var tool string
 	switch {
 	case head.Version != [3]int{0, 0, 0}:
-		tool = fmt.Sprintf("siegfried v%d.%d.%d", head.Version[0], head.Version[1], head.Version[2])
+		tool = fmt.Sprintf("siegfried %d.%d.%d", head.Version[0], head.Version[1], head.Version[2])
 	case head.Identifiers[0][0] == "droid":
 		tool = "droid"
 	case head.Identifiers[0][0] == "fido":
@@ -99,14 +127,52 @@ func results(r *http.Request) (*Results, error) {
 		results.Datas[i] = &Data{
 			FmtCounts:  make(Count),
 			MIMECounts: make(Count),
-			Titles:     make([]string, len(fileTitles)+len(head.Fields[0])),
+			Titles:     make([]string, len(fileTitles)+len(head.Fields[0])-1),
 			Rows:       make([][]string, 0, 1000),
 		}
 		copy(results.Datas[i].Titles, fileTitles)
-		copy(results.Datas[i].Titles[len(fileTitles):], head.Fields[0])
+		copy(results.Datas[i].Titles[len(fileTitles):], head.Fields[0][1:]) // skip the first (ns) field
 	}
+	mimeGetter := getter("mime", head.Fields)
 	for err == nil {
-		// DO PROCESSING STUFF HERE
+		idx := -1
+		var ns string
+		var multiID bool
+		var d *Data
+		for _, id := range file.IDs {
+			if id.Values()[0] != ns {
+				idx++
+				ns = id.Values()[0]
+				multiID = false
+				d = results.Datas[idx]
+			} else {
+				if !multiID {
+					d.Multiple++
+				}
+				multiID = true
+			}
+			d.FmtCounts[id.String()]++
+			d.MIMECounts[mimeGetter(idx, id)]++
+			row := make([]string, len(d.Titles))
+			row[0], row[1], row[2] = file.Path, strconv.FormatInt(file.Size, 10), file.Mod
+			if file.Err != nil {
+				row[3] = file.Err.Error()
+				d.Error++
+			}
+			if id.Warn() != "" {
+				d.Warn++
+			}
+			if !id.Known() {
+				d.Unknown++
+			}
+			slc := 4
+			if hh {
+				row[4] = string(file.Hash)
+				slc = 5
+			}
+			copy(row[slc:], id.Values()[1:])
+			d.Rows = append(d.Rows, row)
+		}
 		file, err = res.Next()
 	}
 	if err == io.EOF {
