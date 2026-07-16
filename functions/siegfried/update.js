3aff0b6fc2866048be77534006316160f268d666
Richard Lehane <richard@itforarchivists.com>
2026-07-17 00:15:46 +1000

    another swing

--- functions/siegfried/update.js
+++ functions/siegfried/update.js
@@ -1,4 +1,4 @@
 export async function onRequestGet(ctx) {
-    const url = new URL("/siegfried/update/update.json", ctx.request.url.origin);
+    const url = new URL("/siegfried/update/update.json", ctx.request.url);
     return await ctx.env.ASSETS.fetch(url);
 }
