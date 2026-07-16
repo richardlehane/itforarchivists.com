04ed64ac6627e7be0d12f09e43b3b2a3d9be8207
Richard Lehane <richard@itforarchivists.com>
2026-07-16 23:46:26 +1000

    another go at update

--- functions/siegfried/update.js
+++ functions/siegfried/update.js
@@ -1,4 +1,4 @@
 export async function onRequestGet(ctx) {
-    const url = new URL("/siegfried/update/update.json");
+    const url = new URL("/siegfried/update/update.json", ctx.request.url.origin);
     return await ctx.env.ASSETS.fetch(url);
 }
